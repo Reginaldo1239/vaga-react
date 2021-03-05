8b52537ccf115c528115f5ff0c931984389374d1
reginaldo melo <reginaldo.melo56@gmail.com>
2021-03-05 15:02:57 -0300

    add o evento onClick

--- ecommerce/src/components/Button/index.js
+++ ecommerce/src/components/Button/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import Style from './index.module.css';
 export default function props(props){
-    let {type,children,otherClasseNames} = props;
+    let {type,children,otherClassNames} = props;
     return(
         <button 
         type={type?type:'button'} 
-        className={classNames(Style.button,otherClasseNames)}>
+        className={classNames(Style.button,otherClassNames)}
+        onClick={(event)=>props.onClick(event)}
+        >
             {children}
         </button>
     )
