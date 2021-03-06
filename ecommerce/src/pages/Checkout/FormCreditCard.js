0860f72ff888e81a2f7bdf655a9e00d6d776e07d
reginaldo melo <reginaldo.melo56@gmail.com>
2021-03-06 13:30:57 -0300

    formulario do cartao de credito criado

---
+++ ecommerce/src/pages/Checkout/FormCreditCard.js
@@ -0,0 +1,171 @@
+import React,{useState} from 'react';
+import classnames from 'classnames';
+import Form from '../../components/Form';
+import FormGroup from '../../components/FormGroup';
+import Input from '../../components/Input';
+import Label from '../../components/Label';
+import {maskCpfOrCnpj, maskCreditCard} from '../../util//mask';
+import LogoMasterCard from '../../assets/logo-mastercard.png';
+import {minLength,cnpjValid,cpfValid} from '../../util/validation';
+import LogoVisa from '../../assets/visa-logo.png';
+import Button from '../../components/Button';
+import Style from './FormCreditCard.module.css'
+import StyleCommon from '../../common.module.css';
+export default function FormCreditCard (props){
+    const initialStateForm ={
+        numberCard:'',
+        nameCard:'',
+        cvvCard:'',
+        flagCard:'',
+        monthCard:'',
+        yearCard:'',
+        cpfOrCnpj:''
+    }
+    const [formValues,setFormValues] = useState(initialStateForm);
+    const [formErrors,setFormErrors] = useState(initialStateForm);
+    const submit=()=>{
+        console.log('submit')
+        if(validForm()){
+
+        }
+     }
+
+
+     const handleInputs=(event)=>{
+        let name = event.currentTarget.name;
+        let value = event.currentTarget.value;
+        if( name!='nameCard'){
+            value=   value.replace(/\D/g,'');
+            switch(name){
+                case 'numberCard':
+                    value = maskCreditCard({flag:null,numberCard:value});
+                    break;
+                case 'monthCard':    
+                    value= value.slice(0,2)
+                        break;
+                case 'yearCard':
+                    value=  value.slice(0,4)
+                        break;
+                case 'cvvCard':
+                    value= value.slice(0,4)
+                        break;
+               case 'cpfOrCnpj':
+                    value = maskCpfOrCnpj({event}) 
+                   break;           
+            }
+        }
+        setFormValues(formValues=>({...formValues ,[name]:value }))
+     }
+     const handleFlag = (flagName)=>{
+           setFormValues(formValues=>({...formValues,'flagCard':flagName})) 
+     }
+        // não validei todas as opções de erro
+     const validForm=()=>{
+        let allErrors =[];
+        let value;
+         for(let name in formValues){
+             value = formValues[name];
+            let  textError ='';
+            if(!minLength(value,1)){
+                textError = `o campo está vázio `;
+            }
+      /*      switch(name){
+                case 'cpfOrCnpj':
+                    //remove o espaço entre os números do cartao
+                   value= value.replace(/\D/g,'');
+                    if(value.length==11 && !cpfValid(value)){
+                        textError = 'cpf inválido';
+                    }else if(value.length==14&& !cnpjValid(value)){
+                        textError = 'cnpj inválido';
+                    }
+                    break;
+                
+            }
+           */
+            if(textError.length>0){
+                allErrors.push({name:textError});
+            }
+            setFormErrors(formErrors=>({ ...formErrors,[name]:textError  }))
+         }
+         
+         return allErrors.length===0;
+     }
+
+  
+
+    return(
+        <Form otherClassNames={Style.form}>
+            <FormGroup>
+                <Label>número do cartão</Label>
+                <Input
+                    name="numberCard"
+                    onChange={handleInputs}
+                    value={formValues.numberCard}
+                    textError={formErrors.numberCard}
+                />
+            </FormGroup>
+            <FormGroup >
+                <Label>nome</Label>
+                <Input
+                    name="nameCard"
+                    onChange={handleInputs}
+                    value={formValues.nameCard}
+                    textError={formErrors.nameCard}
+                />
+            </FormGroup>
+            <div  className={classnames(Style.formGroupCvvAndFlagCard,StyleCommon.flex)}>
+                <div className={Style.cvv}>
+                <Label>cvv</Label>
+                <Input
+                     name="cvvCard"
+                     onChange={handleInputs}
+                     value={formValues.cvvCard}
+                     textError={formErrors.cvvCard}
+                />
+                </div> 
+                <div onClick={()=>handleFlag('visa')} className={classnames(Style.flagCard,{[Style.flagCardActive ]:formValues.flagCard=='visa'})}> 
+                    <img src={LogoVisa}/> 
+                </div>
+                 <div onClick={()=>handleFlag('master-card')} className={classnames(Style.flagCard,{[Style.flagCardActive ]:formValues.flagCard=='master-card'})}>
+                     <img src={LogoMasterCard}/>
+                 </div>
+            </div>
+            <FormGroup otherClassNames={StyleCommon.flex}>
+                <div className={Style.labelValid} >
+                    <Label>valido até</Label>
+                </div>
+                <div>
+                <Input
+                    otherClassNames={Style.inputValid}
+                    placeholder='mês'
+                    name="monthCard"
+                    onChange={handleInputs}
+                    value={formValues.monthCard}
+                    textError={formErrors.monthCard}
+                />
+                </div>
+                <div>
+                <Input
+                    otherClassNames={Style.inputValid}
+                    placeholder='ano'
+                    name="yearCard"
+                    onChange={handleInputs}
+                    value={formValues.yearCard}
+                    textError={formErrors.yearCard}
+                />
+                 </div>
+            </FormGroup>
+            <FormGroup>
+                <Label>cpf/cnpj</Label>
+                <Input
+                    name='cpfOrCnpj'
+                    value={formValues.cpfOrCnpj}
+                    onChange={handleInputs}
+                    onKeyDown={handleInputs}
+                    textError={formErrors.cpfOrCnpj}
+                /> 
+            </FormGroup> 
+            <Button onClick={submit}>confirmar pagamanento</Button>
+        </Form>
+    )
+}
