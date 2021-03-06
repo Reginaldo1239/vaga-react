0aa857b579803b99c0247e0a3f8882b59076a751
reginaldo melo <reginaldo.melo56@gmail.com>
2021-03-06 06:50:41 -0300

    alterei  um prop que estava  com nome errado;

--- ecommerce/src/components/FormGroup/index.js
+++ ecommerce/src/components/FormGroup/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 import Style from './index.module.css';
 export default function (props){
-    let {children,otherClasseNames}=props;
+    let {children,otherClassNames}=props;
     return(
-        <div className={classNames(Style.formGroup,otherClasseNames)} >
+        <div className={classNames(Style.formGroup,otherClassNames)} >
             {children}
         </div>
     )
-}
+} 
