d3735b122a70ea59d4b313e0af9c67d47320336c
reginaldo melo <reginaldo.melo56@gmail.com>
2021-03-06 10:00:26 -0300

    alterei o input para aceitar mensagem de erro

--- ecommerce/src/components/Input/index.js
+++ ecommerce/src/components/Input/index.js
@@ -3,17 +3,25 @@ import classNames from 'classnames';
 
 import Style from './index.module.css';
 export default function Input(props){
-    let {type,name,placeholder,value,otherClassNames} =props;
+    let {type,name,textError,placeholder,value,otherClassNames} =props;
    
         
     return(
+        <div 
+        className={classNames(Style.input,otherClassNames)}
+         >
         <input 
-            className={classNames(Style.input,otherClassNames)}    
             type={type?type:'text'}
             name={name?name:''}
             placeholder={placeholder?placeholder:''}
             value={value?value:''}
             onChange={(event)=>props.onChange(event) }
+            onKeyDown={(event)=>props.onKeyDown?props.onKeyDown(event): null}
             />
+            <div > 
+                {textError}
+            </div> 
+                  
+            </div>
     )
 }
