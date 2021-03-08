10cda0af30a66153803e8922cbd50953e944b921
reginaldo melo <reginaldo.melo56@gmail.com>
2021-03-08 17:31:38 -0300

    api default criada

---
+++ ecommerce/src/services/apiDefault.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3000',
+    headers: {
+        "Content-Type": "application/json",
+        "token":localStorage.getItem('token')|null
+    }
+  });
+export const get=(endPoint)=>{
+   return new Promise((resolver,reject)=>{
+    instance.get(endPoint).then((data)=>{
+            resolver(data);
+        }).catch((err)=>{
+            reject(err)
+        })
+
+   }) 
+}
