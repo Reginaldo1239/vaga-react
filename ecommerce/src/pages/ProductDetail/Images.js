633be4f80779f72b5918a1ae1a8639f3a96bf7b6
reginaldo melo <reginaldo.melo56@gmail.com>
2021-03-09 15:19:24 -0300

    componente que exibe das images da pagina ProductDetail criaod

---
+++ ecommerce/src/pages/ProductDetail/Images.js
@@ -0,0 +1,41 @@
+import React,{useRef, useState,useEffect} from 'react';
+import className from 'classnames'
+import Style from './images.module.css';
+import StyleCommon from '../../common.module.css';
+export default function Images(props){
+    let {infoProduct} = props;
+
+    const [imageMain,setImageMain] =useState();
+    
+    const [listImages,setListImages] = useState([
+        "https://ecommerceteste1.s3.amazonaws.com/product/product-08.jpg",
+        "https://ecommerceteste1.s3.amazonaws.com/product/product-06.jpg"
+    ]);
+    useEffect(()=>{
+        if(infoProduct.imageLocation != undefined){
+                setListImages(listImages=>[infoProduct.imageLocation,...listImages])
+                setImageMain(imageMain=>( infoProduct.imageLocation));
+            }
+    },[infoProduct.imageLocation])
+   
+    const changeImageMain=(imageLocation)=>{
+        setImageMain(imageLocation);
+        
+    }
+    return(
+        <div className={className(Style.images,StyleCommon.flex)}>
+            <div className={Style.listImages}>
+                    {listImages.map((imageLocation,index)=>
+                    <div  
+                    key={index}
+                    onClick={()=>changeImageMain(imageLocation)}
+                    onMouseEnter={()=>changeImageMain(imageLocation)}><img src={imageLocation}></img></div>
+                    )}
+                
+            </div>
+            <div className={Style.singleImage}>
+                <img  src={imageMain}></img>
+            </div>
+        </div>
+    )
+}
